5756cda7157c7744db1614cdccd90fd5f3a0ea14
hguochen <hguochen@gmail.com>
2013-03-31 00:16:47 +0800

    Removed App title in app
    Added android:theme to manifest file.
    Now the app works in fullscreen with no black title bar

--- src/com/example/fullcam/AndroidCamera.java
+++ src/com/example/fullcam/AndroidCamera.java
@@ -28,6 +28,8 @@ import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageButton;
